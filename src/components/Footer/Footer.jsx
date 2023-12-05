d77697382e5b97683ee73d2b9f83e216e8887a37
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-05 16:29:16 -0500

    CeatedFooterComponent

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Footer/footer.css'
+
+function Footer() {
+  return (
+    <div className='Footer-Container'>
+        
+        
+        
+        Footer
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Welcomepage.jsx
+++ src/components/Welcomepage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Footer from './Footer/Footer';
 import {Link} from 'react-router-dom';
 
 function Welcomepage() {
   return (
     <div>
         <h3>Hello welcome to the Food Search recepie</h3>
+        {/* <img src='Food Recipe Finder.png'></img> */}
 
         <Link to='/FoodSearchPage'> Lets Find the Recipe </Link>
-        
+
+        <Footer/>
+
     </div>
   )
 }
