460b57fb4992909e483e2382f48868a6a93b1764
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-06 01:02:13 -0500

    Styled Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Box, Typography} from '@mui/material'
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { PiInstagramLogoFill } from "react-icons/pi";
 import { FaFacebook } from "react-icons/fa";
@@ -6,14 +7,24 @@ import '../Footer/footer.css'
 
 function Footer() {
   return (
-    
-    <div className='Footer-Container'>
+    <>
+    <Box
+        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
+      >
         
       <h3>Follow us on </h3>
         <FaFacebook />
         <AiFillTwitterCircle />
         <PiInstagramLogoFill />
-    </div>
+    
+    <Box>
+    <Typography
+          variant="h7">
+            All Rights Reserved &Copy; Suneetha's Receipe Hunt
+    </Typography>
+    </Box>
+    </Box>
+  </>
   )
 }
 
