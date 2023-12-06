2af12d773504422d900b1523b6193fe8340dad87
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-05 23:08:40 -0500

    Done navBar settings

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { AppBar,Typography,Toolbar,Box,Divider,Drawer,IconButton } from '@mui/material'
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import SRHLogo from '../../models/logo/SRHLogo.png'
 
 const Header = () => {
   return (
     <>
     <Box>
         <AppBar component={'nav'} sx={{ bgcolor: 'black'}}>
-            Recipe Hunter
+        <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <img src={SRHLogo.png} alt="logo" height={"70"} width="250" />
+        </Typography>
         </AppBar>
 
     </Box>
