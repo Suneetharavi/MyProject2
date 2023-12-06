1f256f30efd0154ad091903ff5ada3f7c996c15e
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-06 14:57:16 -0500

    Added image to Homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,36 +7,50 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import { FaFacebook } from "react-icons/fa";
 import '../Footer/footer.css'
 
-function Footer() {
+const Footer = () => {
   // const imageURL="https://t4.ftcdn.net/jpg/03/10/37/25/360_F_310372527_QHGvFcWTuqalbHPabQTmNwJNEQlWeiKv.jpg"
   return (
     <>
     <Box
         sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
       >
-        
-      <h3>Follow us on </h3>
-        <FaFacebook />
-        <AiFillTwitterCircle />
-        <PiInstagramLogoFill />
-        <GitHubIcon />
-        <YouTubeIcon />
-    
-    <Box>
-    <Typography
-          variant="h7"
-          //Styling for responsive design
+        <Box
+        //Styling icons onhover animating to goldcolour
           sx={{
-            "@media (max-width:600px)": {
-              fontSize: "1rem",
-            },
-          }}>
-            All Rights Reserved &copy; Suneetha's Receipe Hunt
-    </Typography>
-    </Box>
-    </Box>
+                my: 3,               //margin:3
+                "& svg": {                  //Targettin svg images directly
+                  fontSize: "60px",
+                  cursor: "pointer",
+                  mr: 2,                //margin right:2
+                },
+                "& svg:hover": {             //Styling onhover
+                  color: "goldenrod",
+                  transform: "translateX(5px)",
+                  transition: "all 400ms",
+                },
+              }}
+          >   
+          <h3>Follow us on </h3>
+            <FaFacebook />
+            <AiFillTwitterCircle />
+            <PiInstagramLogoFill />
+            <GitHubIcon />
+            <YouTubeIcon />
+        </Box>
+  
+      <Typography
+            variant="h7"
+            //Styling for responsive design
+            sx={{
+              "@media (max-width:600px)": {
+                fontSize: "1rem",
+              },
+            }}>
+              All Rights Reserved &copy; Suneetha's Receipe Hunt
+      </Typography>
+    </Box>  
   </>
-  )
-}
+  );
+};
 
 export default Footer
--- src/components/Welcomepage.jsx
+++ src/components/Welcomepage.jsx
@@ -3,22 +3,39 @@ import Footer from './Footer/Footer';
 import {Link} from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Header from './Layout/Header';
+import SRHLogo from '../models/logo/SRHLogo.png'
 
 function Welcomepage() {
   return (
-    <div>
-        <Header/>
-        {/* <img src='Food Recipe Finder.png'></img> */}
-
-        <Layout><h3> Welcome to the Recipe Hunt</h3></Layout>
-
-        <Link to='/FoodSearchPage'> Lets Find the Recipe </Link>
-        {/* <Link to='/About'> About Us</Link>
-        <Link to='/Contact'>Contact</Link> */}
-       
-
+    <Layout>
+  
+    <div className="home" style={{ backgroundImage: `url(${SRHLogo})` }}>
+        <div className="headerContainer">
+          <h1>Food Website</h1>
+          <p>Best Food In India</p>
+          <Link to='/FoodSearchPage'>Receipe Hunting Starts now..  </Link>
+            <button>Lets Find the Recipe!</button>
+        </div>
     </div>
+    
+    </Layout>
+
   )
 }
 
 export default Welcomepage
+
+
+
+    // <div>
+        {/* <Header/>
+        {/* <img src='Food Recipe Finder.png'></img> */}
+
+        {/* <Layout><h3> Welcome to the Recipe Hunt</h3></Layout>
+
+        <Link to='/FoodSearchPage'> Lets Find the Recipe </Link> */}
+        {/* <Link to='/About'> About Us</Link>
+        <Link to='/Contact'>Contact</Link> */} 
+       
+
+    // </div>
