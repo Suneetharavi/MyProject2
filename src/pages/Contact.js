f183bbc84a14be56d390ccebb7c9e5820fea9ab7
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-07 13:44:31 -0500

    styling the box and typography in Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,13 +5,22 @@ import { Box, Typography } from '@mui/material'
 function Contact() {
   return (
     <Layout>
-      <Box>
-          <Typography>
+      <Box sx={{
+        my:10,
+        ml:10,
+        "& h4":{fontWeight:"bold",mb:2}
+      }}>
+          <Typography variant='h4'>
                 Contact Me
           </Typography>
-          
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem odio
+          beatae ducimus magni nobis culpa praesentium velit expedita quae,
+          corrupti, pariatur inventore laboriosam consectetur modi impedit
+          error, repudiandae obcaecati doloribus.
+          </p>
       </Box>
-      <h1>Contact</h1></Layout>
+      </Layout>
   )
 }
 
