53726804c1f4dcc51f62aa4c31aa25a370758e69
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-07 13:40:08 -0500

    added box and Typography in Contact pagez'

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { Box, Typography } from '@mui/material'
 
 function Contact() {
   return (
     <Layout>
+      <Box>
+          <Typography>
+                Contact Me
+          </Typography>
+          
+      </Box>
       <h1>Contact</h1></Layout>
   )
 }
